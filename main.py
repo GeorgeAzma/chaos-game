e1f3162f1d9f3fa22efc7e5acb70744eaffb9c64
GeorgeAzma <gg20060705@gmail.com>
2023-11-01 16:52:49 +0400

    Perfect Commit Commited

--- main.py
+++ main.py
@@ -25,12 +25,14 @@ fs = [
     lambda p: ((p[0] + 1) * 0.5, (p[1] - 1) * 0.5),
     lambda p: ((p[0] + 0) * 0.5, (p[1] + 1) * 0.5),
 ]
+# Rectangle
 fs = [
     lambda p: ((p[0] - 1) * 0.5, (p[1] - 1) * 0.5),
     lambda p: ((p[0] + 1) * 0.5, (p[1] - 1) * 0.5),
     lambda p: ((p[0] + 1) * 0.5, (p[1] + 1) * 0.5),
     lambda p: ((p[0] - 1) * 0.5, (p[1] + 1) * 0.5),
 ]
+# Pentagon
 fs = [
     lambda p: ((p[0] + 0.000) * 0.5, (p[1] + 1.000) * 0.5),
     lambda p: ((p[0] + 0.951) * 0.5, (p[1] + 0.309) * 0.5),
@@ -38,6 +40,26 @@ fs = [
     lambda p: ((p[0] - 0.588) * 0.5, (p[1] - 0.809) * 0.5),
     lambda p: ((p[0] - 0.951) * 0.5, (p[1] + 0.309) * 0.5),
 ]
+# Hexagon
+fs = [
+    lambda p: ((p[0] + 0.000) * 0.5, (p[1] + 1.000) * 0.5),
+    lambda p: ((p[0] + 0.866) * 0.5, (p[1] + 0.500) * 0.5),
+    lambda p: ((p[0] + 0.866) * 0.5, (p[1] - 0.500) * 0.5),
+    lambda p: ((p[0] - 0.000) * 0.5, (p[1] - 1.000) * 0.5),
+    lambda p: ((p[0] - 0.866) * 0.5, (p[1] - 0.500) * 0.5),
+    lambda p: ((p[0] - 0.866) * 0.5, (p[1] + 0.500) * 0.5),
+]
+# Sierpinski carpet
+# fs = [
+#     lambda p: ((p[0] - 1) * 0.33333333333, (p[1] - 1) * 0.33333333333),
+#     lambda p: ((p[0] + 1) * 0.33333333333, (p[1] - 1) * 0.33333333333),
+#     lambda p: ((p[0] + 1) * 0.33333333333, (p[1] + 1) * 0.33333333333),
+#     lambda p: ((p[0] - 1) * 0.33333333333, (p[1] + 1) * 0.33333333333),
+#     lambda p: ((p[0] - 1) * 0.33333333333, (p[1] - 0) * 0.33333333333),
+#     lambda p: ((p[0] + 1) * 0.33333333333, (p[1] - 0) * 0.33333333333),
+#     lambda p: ((p[0] + 0) * 0.33333333333, (p[1] + 1) * 0.33333333333),
+#     lambda p: ((p[0] - 0) * 0.33333333333, (p[1] - 1) * 0.33333333333),
+# ]
 # Barnsley fern
 # range_x = (-2, 2)
 # range_y = (-8, 0)
@@ -69,20 +91,20 @@ while running:
         #     idx = (idx + 1) % len(fs)
 
         # Current vertex can't be previous vertex
-        if idx == prev_idx:
-            idx = (idx + 1) % len(fs)
+        # if idx == prev_idx:
+        #     idx = (idx + 1) % len(fs)
 
         # Current vertex can't be opposite to previous vertex
         # if idx == len(fs) - 1 - prev_idx:
         #     idx = (idx + 1) % len(fs)
 
         # Current vertex can't be neighboring previous vertex
-        # if (
-        #     abs(prev_idx - idx) == 1
-        #     or (prev_idx == 0 and idx == len(fs) - 1)
-        #     or (idx == 0 and prev_idx == len(fs) - 1)
-        # ):
-        #     idx = (idx + 1) % len(fs)
+        if (
+            abs(prev_idx - idx) == 1
+            or (prev_idx == 0 and idx == len(fs) - 1)
+            or (idx == 0 and prev_idx == len(fs) - 1)
+        ):
+            idx = (idx + 1) % len(fs)
 
         # Current vertex can't be neighboring previous vertex if previous vertex is equal to previous to previous vertex
         # if prev2_idx == prev_idx and (
@@ -99,7 +121,18 @@ while running:
         if it > 20:
             sx = int((p[0] - range_x[0]) / range_width * size[0])
             sy = int((-p[1] - range_y[0]) / range_height * size[1])
-            screen.set_at((sx, sy), (sx / size[0] * 255, sy / size[1] * 255, 255))
+            sx = min(size[0] - 1, max(0, sx))
+            sy = min(size[1] - 1, max(0, sy))
+            px = screen.get_at((sx, sy))
+            if px[0] + px[1] + px[2] < 510:
+                screen.set_at(
+                    (sx, sy),
+                    (
+                        max(0, min(255, px[0] + 4 + sx / size[0] * 16)),
+                        max(0, min(255, px[1] + 4 + sy / size[1] * 16)),
+                        max(0, min(255, px[2] + 4)),
+                    ),
+                )
         it += 1
 
     # Process Events
